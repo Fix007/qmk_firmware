d907db70ba338fc5cb56de527e93c522ad867cb4
Felix Pries <34555911+Fix007@users.noreply.github.com>
2023-03-23 17:09:44 +0100

    add new keymap

--- keyboards/fix007/swoop/keymaps/default/keymap.c
+++ keyboards/fix007/swoop/keymaps/default/keymap.c
@@ -39,4 +39,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                    KC_LCTL, KC_BSPC, KC_ESC,      KC_ENT,  KC_SPC,  KC_LALT
 )
+// [_QWERTY] = LAYOUT_split_3x5_3(
+//   _______, RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
+//   _______, RGB_M_B, RGB_M_P, RGB_HUI, RGB_HUD, _______, _______, _______, _______, _______,
+//   QK_BOOT, RGB_M_R, RGB_TOG, RGB_SAI, RGB_SAD, _______, _______, QK_BOOT,  _______, _______,
+//                     _______, _______, _______,        _______, _______, _______
+// )
 };
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    render_logo();
+    return false;
+}
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
--- keyboards/fix007/swoop/rules.mk
+++ keyboards/fix007/swoop/rules.mk
@@ -10,10 +10,15 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 RGBLIGHT_ENABLE = yes       # Enable underlight
 SPLIT_KEYBOARD = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+# ENCODER_ENABLE = no
+OLED_DRIVER_ENABLE = yes
+# WPM_ENABLE = yes
 
 LAYOUTS = split_3x5_3
